aafe18cfa0fcd1fdbdead2cdd38655634435f16a
Ollie Power <powerollie@gmail.com>
2021-04-12 11:45:51 +0100

    Added some notes from datetime lecture

--- course_notes.py
+++ course_notes.py
@@ -181,3 +181,45 @@ cheeseshop("Limburger", "It's very runny, sir.",
 # This is often used either to provide a convenient user interface to a module,
 #  or for testing purposes (running the module as a script executes a test suite).
 print(__name__)
+
+import copy
+
+person_1 = {'name': 'Walter Wall', "age": 35, "email": "walter_wall@gmail.com", "profession": "floorer"}
+person_2 = {'name': 'Polly Pocket', 'age': 6, 'email': 'polly_pocket@hotmail.com', 'profession': 'Children\'s Entertainer'}
+customers = {1234: person_1, 2345: person_2}
+customers_deep_copy = copy.deepcopy(customers)
+customers_shallow_copy = customers.copy()
+person_1.clear()
+print(f"Customers Person 1: \n {customers[1234]}")
+print(f"Customers Copy Person 1: \n {customers_shallow_copy[1234]}")
+print(f"Customers Copy Person 1: \n {customers_deep_copy[1234]}")
+
+# https://docs.python.org/3/library/datetime.html#module-datetime
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+import datetime as dt
+# method strftime of datetime class in module dt
+#formatted_dt = dt.datetime.strftime(datetime_object, "format codes")
+
+current_time = dt.datetime.now()
+print(f"Unformatted date and time: {current_time}")
+formatted_time = dt.datetime.strftime(current_time, "%d/%m/%Y %H:%M:%S")
+print(f"Formatted date and time: {formatted_time}")
+
+# method strftime of date class in module dt
+#formatted_dt = dt.datetime.strftime(datetime_object, "format codes")
+
+current_date = dt.date.today()
+print(f"Unformatted date: {current_date}")
+formatted_date = dt.date.strftime(current_date, "%a, %d %b %y")
+print(f"Formatted date: {formatted_date}")
+
+# new_datetime_object = dt.datetime.strptime("string to parse", "format codes")
+
+current_date = dt.date.today()
+input_date = input("Enter the date of your deadline in the format dd/mm/yyyy format: ")
+deadline = dt.datetime.strptime(input_date, "%d/%m/%Y")
+print(f"Your deadline (datetime): {deadline}")
+# convert datetime to date
+deadline = deadline.date()
+print(f"Your deadline (date): {deadline}")
+print(f"You have {(deadline - current_date).days} days until your deadline.")
